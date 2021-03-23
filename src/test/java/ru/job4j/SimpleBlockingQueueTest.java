e57c63bde2590175ee088fb05963c371c50a6b51
plifis <nikich85@inbox.ru>
2021-03-23 19:52:59 +0300

    3. Junit тест для блокирующей очереди. [#493043]

--- src/test/java/ru/job4j/SimpleBlockingQueueTest.java
+++ src/test/java/ru/job4j/SimpleBlockingQueueTest.java
@@ -1,66 +1,97 @@
-//package ru.job4j;
-//
-//import org.apache.log4j.pattern.LineSeparatorPatternConverter;
-//import org.junit.Test;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//import java.util.Queue;
-//
-//import static org.hamcrest.MatcherAssert.assertThat;
-//import static org.junit.Assert.assertEquals;
-//
-//
-//public class SimpleBlockingQueueTest {
-//    @Test
-//    public void add() throws InterruptedException {
-//        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(3);
-//        Thread offer = new Thread (
-//                () -> { for (int i = 0; i < 10; i++) {
-//                    queue.offer(i);
-//                    System.out.println("Добавлено" + i);
-//                }
-//        }
-//        );
-//        Thread poll = new Thread(
-//                () -> {
-//                    for (int i = 0; i < 10; i++) {
-//                        System.out.println("Получено" +  queue.poll());
-//                    }
-//                }
-//        );
-//        offer.start();
-//        poll.start();
-//        offer.join();
-//        poll.join();
-//    }
-//
-//    @Test
-//    public void whenAdd3ThenPoll3() throws InterruptedException {
-//        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(3);
-//        List<Integer> list = List.of(1, 2, 3, 4, 5, 6);
-//        List<Integer> rsl = new ArrayList<>();
-//        Thread offer = new Thread (
-//                () -> { for (int i : list) {
-//                        queue.offer(i);
-//                    System.out.println("Добавлено" + i);
-//                }
-//        }
-//        );
-//        Thread poll = new Thread(
-//                () -> {
-//                    while (!queue.isEmpty()) {
-//                        int i = queue.poll();
-//                        System.out.println("Получено" + i);
-//                        rsl.add(i);
-//                    }
-//                }
-//        );
-//        offer.start();
-//        poll.start();
-//        offer.join();
-//        poll.join();
-//        assertEquals(list, rsl);
-//    }
-//
-//}
+package ru.job4j;
+
+import org.apache.log4j.pattern.LineSeparatorPatternConverter;
+import org.junit.Test;
+
+import java.util.*;
+import java.util.stream.IntStream;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertEquals;
+
+
+public class SimpleBlockingQueueTest {
+    @Test
+    public void add() throws InterruptedException {
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(3);
+        Thread offer = new Thread (
+                () -> { for (int i = 0; i < 10; i++) {
+                    queue.offer(i);
+                    System.out.println("Добавлено" + i);
+                }
+        }
+        );
+        Thread poll = new Thread(
+                () -> {
+                    for (int i = 0; i < 10; i++) {
+                        System.out.println("Получено" +  queue.poll());
+                    }
+                }
+        );
+        offer.start();
+        poll.start();
+        offer.join();
+        poll.join();
+    }
+
+    @Test
+    public void whenAdd3ThenPoll3() throws InterruptedException {
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(3);
+        List<Integer> list = List.of(1, 2, 3, 4, 5, 6);
+        List<Integer> rsl = new ArrayList<>();
+        Thread producer = new Thread (
+                () -> { for (int i : list) {
+                        queue.offer(i);
+                    System.out.println("Добавлено" + i);
+                }
+        }
+        );
+        producer.start();
+        Thread consumer = new Thread(
+                () -> {
+                    while (!queue.isEmpty() || !Thread.currentThread().isInterrupted()) {
+                           int i = queue.poll();
+                           System.out.println("Получено" + i);
+                           rsl.add(i);
+                    }
+                }
+        );
+        consumer.start();
+        producer.join();
+        consumer.interrupt();
+        consumer.join();
+        assertEquals(list, rsl);
+    }
+    @Test
+    public void whenFetchAllThenGetIt() throws InterruptedException {
+        final List<Integer> buffer = new LinkedList<>();
+        final SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>();
+        Thread producer = new Thread(
+                () -> {
+                    IntStream.range(0, 5).forEach(
+                            queue::offer
+                    );
+                }
+        );
+        producer.start();
+        Thread consumer = new Thread(
+                () -> {
+                    while (!queue.isEmpty() || !Thread.currentThread().isInterrupted()) {
+                           try {
+                               buffer.add(queue.poll());
+                               Thread.sleep(500);
+                           } catch (InterruptedException e) {
+                               Thread.currentThread().interrupt();
+                           }
+                    }
+                }
+        );
+        consumer.start();
+        producer.join();
+        consumer.interrupt();
+        consumer.join();
+        assertThat(buffer, is(Arrays.asList(0, 1, 2, 3, 4)));
+    }
+
+}
